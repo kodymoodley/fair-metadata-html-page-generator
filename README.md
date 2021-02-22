62e8db82ecda36783200956db1453e72c794fc23
kody moodley <kody.moodley@gmail.com>
2021-02-22 15:52:08 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# fair-metadata-html-page-generator
-A simple Python script to generate a simple static HTML page for viewing a JSON-LD metadata description of a dataset using Schema.org markup vocabulary.
+# [FAIR](https://www.go-fair.org/fair-principles/) metadata html page generator
+
+### Description
+
+A simple Python script to generate a static [HTML](https://html.spec.whatwg.org/) page for viewing a [JSON-LD](https://json-ld.org/) metadata description of a dataset using [Schema.org](https://schema.org/) markup vocabulary.
+
+[Structured](https://developers.google.com/search/docs/guides/intro-structured-data), machine-readable metadata about datasets are helpful for improving the [Findability, Accessibility, Interoperability and Reusability](https://www.go-fair.org/fair-principles/). [Google](https://www.google.com/), for example, is gradually developing support for [dataset search tools](https://datasetsearch.research.google.com/) to help researchers find and reuse relevant data. Providing machine-readable descriptions of datasets is helpful for search engine indexing software to process and index.
+
+I have developed a Python script [here](https://github.com/kodymoodley/fair-metadata-generator) to generate such a description in JSON-LD using [Schema.org](https://schema.org/) markup vocabulary. 
+
+However, after this JSON-LD description is generated, it is often useful to publish a webpage describing the content of the JSON-LD file for viewing by humans. Given an input JSON-LD file describing a dataset, the script ``jsonld_to_html.py`` generates a single static HTML page accomplishing this.
+
+### Requirements
+
++ [Python 3.7+](https://www.python.org/downloads/)
++ Python libraries specified in ``requirements.txt`` in this repository
++ A JSON-LD file compliant with the [Schema.org](https://schema.org/) [Dataset](https://schema.org/Dataset) entity description. If you do not have one of these files at hand, you can generate one using [this tool](https://github.com/kodymoodley/fair-metadata-generator). Alternatively, there is an example file under ``testdata/inputdata/`` in this repository.
+
+To install the required libraries in ``requirements.txt`` run: ``pip install -r requirements.txt`` in your command line environment (after installing Python).
+
+### Usage
+
+Run ``python jsonld_to_html.py <relative path to input json-ld file> <relative path to output html file>`` in your command line environment after installing Python and the required libraries. For example: ``python jsonld_to_html.py testdata/inputdata/test.jsonld testdata/outputdata/test.html``
+
+**Note:** the styling of the output HTML page(s) is defined in the ``style.css`` [CSS](https://www.w3.org/TR/css-2020/) file located in ``testdata/outputdata/``. If you prefer a different styling, this is the file to modify. The output HTML files also expect the CSS file to be located in a folder called ``css/`` in the same directory as the output HTML file(s). 
+
+### Developer notes
+
+This tool makes use of the [Jinja](https://jinja.palletsprojects.com/en/2.11.x/) templating engine for Python. I have defined an HTML template ``templates/html_template.html`` in this repository. If you would like to customise your HTML files for viewing the JSON-LD metadata, you can modify this template or add new custom templates in this folder. You would then need to update references to this template file in ``jsonld_to_html.py`` and also make necessary modifications to the ``mappings/jsonld_to_htmltemplate_mapping.csv`` file. This file defines a mapping between JSON-LD keys for Schema.org [datasets](https://schema.org/Dataset) and the Jinja variables (in the HTML template file) that should hold the values for these keys. I plan to include the mapping file and the HTML template as arguments for the ``json_to_html.py`` script in the near future.
+
+### License and contributions
+
+The FAIR metadata html page generator is copyrighted by [Kody Moodley](https://sites.google.com/site/kodymoodley/) and released under the [GNU Affero License](https://www.gnu.org/licenses/agpl-3.0.txt)
+
+Contributions and bug reports are helpful and welcome.
+
---
+++ jsonld_to_html.py
@@ -0,0 +1,187 @@
+import json
+from html.parser import HTMLParser
+import pandas as pd
+import sys
+
+inputfile = ""
+outputfile = ""
+data = ""
+
+if (len(sys.argv) == 2):
+    if (str(sys.argv[1]).lower() not in ['-h', 'h', 'help', '-help']):
+        print("Incorrect or too few argument(s). Please type -h for help.")
+        exit()
+    else:
+        print("usage: python jsonld_to_html.py <path to input json-ld file> <path to output html file>")
+        exit()
+elif (len(sys.argv) == 3):
+    try:
+        # Load input data file
+        inputfile=sys.argv[1]
+        with open(sys.argv[1]) as json_file:
+            data = json.load(json_file) 
+    except Exception as e:
+        print("Input file is not a valid JSON file.")
+        exit()
+    try:
+        # Valid output file path
+        f = open(sys.argv[2], "w")
+    except Exception as e:
+            print("Output file path is not valid.")
+            exit()
+    outputfile = sys.argv[2]
+else:
+    print("Incorrect number of argument(s). Please type -h for help.")
+    exit()
+
+# Load data
+with open(inputfile) as json_file:
+    data = json.load(json_file)   
+    
+# Load JSON-LD-key to HTML-template-variable mappings
+df = pd.read_csv ("mappings/jsonld_to_htmltemplate_mapping.csv", index_col=None)
+
+
+# In[142]:
+
+
+# Software metadata
+software_var = {}
+# Dependency dataset metadata
+depdataset_var = {}
+
+# Helper function: Resolve JSON-LD-key to HTML-template-variable mappings
+def resolve_value(key, jsondata):
+    keyparts = key.split(";")
+    if (len(keyparts) == 1):
+        return jsondata[keyparts[0]]
+    else:
+        jsondata=jsondata[keyparts[0]]
+        new_key = ";".join(keyparts[1:])
+        if keyparts[1] in jsondata:
+            return resolve_value(new_key, jsondata)
+        else:
+            return ""
+        
+# Resolve JSON-LD-key to HTML-template-variable mappings  
+def get_value(input_variable):
+    global data
+    global df
+    
+    json_key = df[df['html_template_variable']==input_variable]['jsonld_key'].values[0]
+    keyparts = json_key.split(";")
+    if keyparts[0] in data:
+        result = resolve_value(json_key, data)
+        return result
+    else:
+        return ""
+    
+# For isBasedOn property: special case
+def get_new_key(input_variable):
+    global df
+    
+    json_key = df[df['html_template_variable']==input_variable]['jsonld_key'].values[0]
+    return json_key.replace("isBasedOn;","")
+
+# Obtain related software or data dependencies from isBasedOn property
+def handle_isBasedOn():
+    global data
+    global software_var
+    global depdataset_var
+    
+    if "isBasedOn" in data:
+        if len(data["isBasedOn"]) > 0:
+            for item in data["isBasedOn"]:
+                if (item["@type"] == "SoftwareApplication"):
+                    software_var = item
+                else:
+                    depdataset_var = item
+
+# Handle isBasedOn property
+handle_isBasedOn()
+
+import jinja2
+
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = "templates/html_template.html"
+template = templateEnv.get_template(TEMPLATE_FILE)
+outputText = template.render(
+
+# Dataset
+jsonld=data,
+datasetname=get_value('datasetname'),
+datasetdescription=get_value('datasetdescription'),
+datasetversion=get_value('datasetversion'),
+datasetencodingFormat=get_value('datasetencodingFormat'),
+datasetdateCreated=get_value('datasetdateCreated'),
+datasetdatePublished=get_value('datasetdatePublished'),
+datasetlicense=get_value('datasetlicense'),
+dataseturl=get_value('dataseturl'),
+datasetinLanguage=get_value('datasetinLanguage'),
+datasettemporal=get_value('datasettemporal'),
+datasettemporalCoverage=get_value('datasettemporalCoverage'),
+datasetkeywords=get_value('datasetkeywords'),
+    
+# Author
+authorimage=get_value('authorimage'),
+authorname=get_value('authorname'),
+authorjobTitle=get_value('authorjobTitle'),
+authoremail=get_value('authoremail'),
+affiliationname=get_value('affiliationname'),
+affiliationurl=get_value('affiliationurl'),
+organizationimageurl=get_value('organizationimageurl'),
+
+# Contributor
+contributorlist=get_value('contributorlist'),
+
+# Publisher
+publisherorganizationname=get_value('publisherorganizationname'),
+publisherorganizationlogo=get_value('publisherorganizationlogo'),
+publisherorganizationurl=get_value('publisherorganizationurl'),
+publisherpersonname=get_value('publisherpersonname'),
+publisherpersonjobTitle=get_value('publisherpersonjobTitle'),
+publisheremail=get_value('publisheremail'),
+publisherpersonaffiliationname=get_value('publisherpersonaffiliationname'),
+publisherpersonaffiliationurl=get_value('publisherpersonaffiliationurl'),
+publisherpersonaffiliationimage=get_value('publisherpersonaffiliationimage'),
+
+# Download
+downloadurl=get_value('downloadurl'),
+downloadencodingFormat=get_value('downloadencodingFormat'),
+downloadcontentSize=get_value('downloadcontentSize'),
+
+# Software
+softwarename=resolve_value(get_new_key('softwarename'), software_var),
+softwaredescription=resolve_value(get_new_key('softwaredescription'), software_var),
+softwarecategory=resolve_value(get_new_key('softwarecategory'), software_var),
+softwareos=resolve_value(get_new_key('softwareos'), software_var),
+softwareversion=resolve_value(get_new_key('softwareversion'), software_var),
+softwareurl=resolve_value(get_new_key('softwareurl'), software_var),
+
+# Dependency dataset
+depdatasetname=resolve_value(get_new_key('depdatasetname'), depdataset_var),
+depdatasetdescription=resolve_value(get_new_key('depdatasetdescription'), depdataset_var),
+depdatasetversion=resolve_value(get_new_key('depdatasetversion'), depdataset_var),
+depdataseturl=resolve_value(get_new_key('depdataseturl'), depdataset_var),
+
+# Publication
+publicationname=get_value('publicationname'),
+publicationauthorlist=get_value('publicationauthorlist'),
+publicationpublishername=get_value('publicationpublishername'),
+publicationdatePublished=get_value('publicationdatePublished'),
+publicationsameAs=get_value('publicationsameAs'),
+publicationpublisherurl=get_value('publicationpublisherurl')
+
+)
+
+#print(outputText)
+
+# Save result to file
+with open(outputfile, "w") as fh:
+    fh.write(outputText)
+
+
+
+
+
